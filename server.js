0fa69435bdb695ae82c4f60ec05fc84694852733
muiradams <muir@humdig.com>
2017-03-11 21:48:01 -0800

    update database files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const postgraphql = require('postgraphql').postgraphql;
 const app = express();
 const port = process.env.PORT || 4000;
 // TODO: needs to point to location of database on server for production
-const DB_URI = 'postgres://localhost:5432/flights';
+const DB_URI = 'postgres://localhost:5432/routetogo';
 
 app.use(postgraphql(DB_URI, {
   disableDefaultMutations: true,
